0f94c3a4db2ae700c477ec326d0bc2a276013289
Qingsong.You <qsyou@thoughtworks.com>
2016-09-08 14:18:11 +0800

    create a repository service and its impl

---
+++ core/src/main/java/com/github/qingsong/core/RepositoryService.java
@@ -0,0 +1,8 @@
+package com.github.qingsong.core;
+
+/**
+ * Created by qsyou on 9/8/16.
+ */
+public interface RepositoryService {
+    public String getRepositoryName();
+}
---
+++ core/src/main/java/com/github/qingsong/core/impl/RepositoryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.github.qingsong.core.impl;
+
+
+import com.github.qingsong.core.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Reference;
+
+import javax.jcr.Repository;
+
+@Component
+
+@Service(value = RepositoryService.class)
+public class RepositoryServiceImpl implements RepositoryService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryServiceImpl.class);
+
+    @Reference
+    private Repository repository;
+
+    public String getRepositoryName() {
+        return repository.getDescriptor(Repository.REP_NAME_DESC);
+    }
+
+    @Activate
+    protected void activate() {
+        LOGGER.info("========== service activated");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        LOGGER.info("============service deactivated");
+    }
+}
